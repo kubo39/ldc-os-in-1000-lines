88f17f3d3a1d973a82d44fb845c5652d1d470e97
Hiroki Noda <kubo39@gmail.com>
2023-12-08 20:28:37 +0900

    Hello, World!

--- kernel.d
+++ kernel.d
@@ -7,6 +7,27 @@ extern __gshared char* __bss;
 extern __gshared char* __bss_end;
 extern __gshared char* __stack_top;
 
+struct sbiret
+{
+    long error;
+    long value;
+}
+
+sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
+                long arg5, long fid, long eid)
+{
+    return __asm!sbiret(
+        "ecall",
+        "={a0},={a1},{a0},{a1},{a2},{a3},{a4},{a5},{a6},{a7},~{memory}",
+        arg0, arg1, arg2, arg3, arg4, arg5, fid, eid
+    );
+}
+
+void putchar(char ch)
+{
+    sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
+}
+
 void* memset(void* buf, char c, size_t n)
 {
     char* p = cast(char*) buf;
@@ -20,7 +41,14 @@ void* memset(void* buf, char c, size_t n)
 void kernel_main()
 {
     memset(__bss, 0, cast(size_t) __bss_end - cast(size_t) __bss);
-    for (;;) {}
+    const(char)* s = "\n\nHello World!\n";
+    for (int i = 0; s[i] != '\0'; i++) {
+        putchar(s[i]);
+    }
+    for (;;)
+    {
+        __asm("wfi", "");
+    }
 }
 
 @section(".text.boot")
