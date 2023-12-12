7a7dcee158f7bd81c3161a3d2207894744468ec8
Hiroki Noda <kubo39@gmail.com>
2023-12-12 14:26:27 +0900

    printf関数をcommonモジュールに移動

--- common.d
+++ common.d
@@ -2,6 +2,91 @@ module common;
 
 extern (C):
 
+alias va_list = imported!"core.stdc.stdarg".va_list;
+alias va_start = imported!"core.stdc.stdarg".va_start;
+alias va_end = imported!"core.stdc.stdarg".va_end;
+alias va_arg = imported!"core.stdc.stdarg".va_arg;
+
+void putchar(char);
+
+void printf(const(char)* fmt, ...)
+{
+    va_list vargs;
+    va_start(vargs, fmt);
+
+    while (*fmt)
+    {
+        if (*fmt == '%')
+        {
+            fmt++;
+            switch (*fmt)
+            {
+            case '\0':
+                putchar('%');
+                goto end;
+            case '%':
+                putchar('%');
+                break;
+            case 's':
+            {
+                const(char)* s = va_arg!(const(char)*)(vargs);
+                while (*s)
+                {
+                    putchar(*s);
+                    s++;
+                }
+                break;
+            }
+            case 'd':
+            {
+                int value = va_arg!int(vargs);
+                if (value < 0)
+                {
+                    putchar('-');
+                    value = -value;
+                }
+
+                int divisor = 1;
+                while (value / divisor > 9)
+                {
+                    divisor *= 10;
+                }
+
+                while (divisor > 0)
+                {
+                    putchar('0' + value / divisor);
+                    value %= divisor;
+                    divisor /= 10;
+                }
+
+                break;
+            }
+            case 'x':
+            {
+                int value = va_arg!int(vargs);
+                for (int i = 7; i >= 0; i--)
+                {
+                    int nibble = (value >> (i * 4)) & 0xf;
+                    putchar("0123456789abcdef"[nibble]);
+                }
+                break;
+            }
+            default:
+                break;
+            }
+        }
+        else
+        {
+            putchar(*fmt);
+        }
+
+        fmt++;
+    }
+
+end:
+    va_end(vargs);
+}
+
 void* memset(void* buf, char c, size_t n)
 {
     ubyte* p = cast(ubyte*) buf;
